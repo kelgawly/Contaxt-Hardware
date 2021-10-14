61217f6e6bfdfb682df904a1554f8e116f9701fa
Alex Waldron <awaldro1@stevens.edu>
2021-10-14 14:43:10 -0400

    fixed bug in firmware code

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,10 +1,10 @@
-
 // Include Libraries
 #include "Arduino.h"
 #include "ADXL345.h"
 #include "I2Cdev.h"
 #include "FSR.h"
 #include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
 // Pin Definitions
 #define FSRSQUARE_PIN_1	A0
@@ -15,19 +15,18 @@
 
 // Global variables and defines
 int16_t adxlAx, adxlAy, adxlAz;
+TinyGPSPlus gps;
+
 // object initialization
 ADXL345 adxl;
 FSR fsrSquare(FSRSQUARE_PIN_1);
 
-// Choose two Arduino pins to use for software serial
-int RXPin = 10; //TODO: make sure these pins are correct
-int TXPin = 11;
 
 //Default baud of NEO-6M is 9600
 int GPSBaud = 9600;
 
 // Create a software serial port called "gpsSerial"
-SoftwareSerial gpsSerial(RXPin, TXPin);
+SoftwareSerial gpsSerial(GPS_PIN_RX, GPS_PIN_TX);
 
 
 // define vars for testing menu
@@ -77,19 +76,29 @@ void loop()
     }
     else if(menuOption == '3')
     {
-    // Disclaimer: The Ublox NEO-6M GPS Module is in testing and/or doesn't have code, therefore it may be buggy. Please be kind and report any bugs you may find.
+    // Disclaimer: The Ublox NEO-6M GPS Module is outdated, we might need to buy the 8M for continued support
     // for more information visit: https://lastminuteengineers.com/neo6m-gps-arduino-tutorial/
     //print out 10 readings from the GPS module
     // returns NMEA sentences which need to be parsed, visit the site above and section "Parsing NMEA Sentences" for more information
     //TinyGPS++ seems to be a good library for doing this
     int counter = 0;
-      while (gpsSerial.available() > 0){
-        Serial.write(gpsSerial.read());
-        counter ++;
-        if (counter >= 10){
-          break;
-        }
+    while (gpsSerial.available() > 0){
+    if (gps.encode(gpsSerial.read())){
+      counter ++;
+      displayInfo();
+      if(counter >= 10){
+        break;
       }
+    }
+    }
+
+    // If 5000 milliseconds pass and there are no characters coming in
+    // over the software serial port, show a "No GPS detected" error
+    if (millis() > 5000 && gps.charsProcessed() < 10)
+    {
+      Serial.println("No GPS detected");
+      while(true);
+    }
     }
     
     if (millis() - time0 > timeout)
@@ -125,7 +134,7 @@ char menu()
     		else if(c == '2') 
     			Serial.println(F("Now Testing Force Sensitive Resistor - Square"));
     		else if(c == '3') 
-    			Serial.println(F("Now Testing Ublox NEO-6M GPS Module - note that this component doesn't have a test code"));
+    			Serial.println(F("Now Testing Ublox NEO-6M GPS Module"));
             else
             {
                 Serial.println(F("illegal input!"));
@@ -135,6 +144,63 @@ char menu()
             return c;
         }
     }
+}
+
+
+//display interface for GPS readings
+void displayInfo()
+{
+  if (gps.location.isValid())
+  {
+    Serial.print("Latitude: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(), 6);
+    Serial.print("Altitude: ");
+    Serial.println(gps.altitude.meters());
+  }
+  else
+  {
+    Serial.println("Location: Not Available");
+  }
+  
+  Serial.print("Date: ");
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print("/");
+    Serial.print(gps.date.day());
+    Serial.print("/");
+    Serial.println(gps.date.year());
+  }
+  else
+  {
+    Serial.println("Not Available");
+  }
+
+  Serial.print("Time: ");
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(":");
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(":");
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(".");
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.println(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.println("Not Available");
+  }
+
+  Serial.println();
+  Serial.println();
+  delay(1000);
 }
 
 /*******************************************************
